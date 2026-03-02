7d0a94782f757308f4f6134f6a18c7f24a13cf17
Sandrine P <sandrine.pham.89@gmail.com>
2026-03-02 20:00:50 +0100

    modif chemin dan App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.jsx";
 import { ProfileProvider } from "./context/profile/ProfileContext.jsx";
 import { ContentProvider } from "./content/ContentProvider.jsx";
-import "./app.scss";
+import "./App.scss";
 
 export default function App() {
   return (
